47c86bd9ca7143400d92ccee72b56260f472dddf
RNS Technology <rnstechnologysg@gmail.com>
2026-08-05 12:50:11 +0800

    feat: add Project Experience scrolling logo marquee to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
 import { Hero } from "@/components/sections/Hero";
+import { ProjectExperienceSection } from "@/components/sections/ProjectExperienceSection";
 import { TrainTrackTimelineSection } from "@/components/sections/TrainTrackTimelineSection";
 import { IndustriesSection } from "@/components/sections/IndustriesSection";
 import { ServicesSection } from "@/components/sections/ServicesSection";
@@ -14,6 +15,7 @@ export default function Home() {
       <Navbar />
       <main className="flex-1 bg-white">
         <Hero />
+        <ProjectExperienceSection />
         <TrainTrackTimelineSection />
         <IndustriesSection />
         <WorkforceProcessSection />
